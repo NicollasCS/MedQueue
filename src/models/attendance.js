76e8cb4652c42e031821051d5a62b1e3288c767a
NICOLLAS CANE SEULA <nicollas_seula@estudante.sesisenai.org.br>
2026-04-29 15:55:10 -0300

    code models/attendance.js

---
+++ src/models/attendance.js
@@ -0,0 +1,55 @@
+const database = require('../config/database');
+const { DataTypes } = require('sequelize');
+const Patient = require('./patient');
+
+const Attendance = database.define(
+  'Attendance',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    patientId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Patient,
+        key: 'id',
+      },
+    },
+    prioridade: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    status: {
+      type: DataTypes.ENUM('Aguardando', 'Em Atendimento', 'Finalizado'),
+      defaultValue: 'Aguardando',
+    },
+    data_entrada: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    data_atendimento: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    data_finalizacao: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'atendimentos',
+    timestamps: true,
+  },
+);
+
+Patient.hasMany(Attendance, { foreignKey: 'patientId' });
+Attendance.belongsTo(Patient, { foreignKey: 'patientId' });
+
+module.exports = Attendance;
